ddcfafeb2575c65713b7cb43e7ca76ffa406215e
kristianmikhael667 <kristianmikhael667@gmail.com>
2022-04-28 23:14:34 +0700

    se

--- resources/views/admin/create-community-video.blade.php
+++ resources/views/admin/create-community-video.blade.php
@@ -57,7 +57,7 @@
                                                 </div>
                                                 <div class="uk-margin">
                                                     <div class="uk-margin" id="imagepost">
-                                                        <input type="file" name="avatar" id="avatar" class="dropify"
+                                                        <input type="file" name="thumbnails" id="thumbnails" class="dropify"
                                                             data-max-file-size="5M">
                                                     </div>
                                                 </div>

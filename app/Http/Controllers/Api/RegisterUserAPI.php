15446f995920c1d56708a360713cea76ed981c31
kristianmikhael667 <kristianmikhael667@gmail.com>
2022-05-09 10:19:01 +0700

    dew

--- app/Http/Controllers/Api/RegisterUserAPI.php
+++ app/Http/Controllers/Api/RegisterUserAPI.php
@@ -7,11 +7,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class RegisterUserAPI extends Controller
 {
-    public function sendVerificationEmail(Request $request){
-        if($request->user()->hasVerifiedEmail()){
+    public function sendVerificationEmail(Request $request)
+    {
+        if ($request->user()->hasVerifiedEmail()) {
             return ResponseFormatter::error(
                 null,
                 'Already Verified'
@@ -24,19 +26,21 @@ class RegisterUserAPI extends Controller
         );
     }
 
-    public function verify(EmailVerificationRequest $request){
-        if($request->user()->hasVerifiedEmail()){
+    public function verify(EmailVerificationRequest $request)
+    {
+        if ($request->user()->hasVerifiedEmail()) {
             return ResponseFormatter::error(
                 null,
                 'Email Already Verified'
             );
         }
-        if($request->user()->markEmailAsVerified()){
+        if ($request->user()->markEmailAsVerified()) {
             event(new Verified($request->user()));
         }
         return ResponseFormatter::success(
             null,
             'Email has been Verified'
         );
+        return Redirect::to('http://front.dewanhoster.my.id/login');
     }
 }
